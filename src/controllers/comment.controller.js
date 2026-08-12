40cc6a0fbb1b154da274a8297c8d007e5eafc090
OhadRaza01 <ohadraza345@gmail.com>
2026-08-13 02:41:42 +0500

    import mongoose in comment controller for object ID validation

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -3,6 +3,7 @@ import asyncHandler from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Video } from "../models/video.model.js";
+import mongoose from "mongoose";
 
 const addComment = asyncHandler(async (req, res) => {
 
