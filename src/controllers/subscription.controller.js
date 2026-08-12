c112b4114e6766f8fdee4d073ef30309b69b0051
OhadRaza01 <ohadraza345@gmail.com>
2026-08-12 08:06:05 +0500

    added get subscribed channel endpoint

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -111,4 +111,60 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
         )
 })
 
-export { toggleSubscription, getUserChannelSubscribers }
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    
+    const { subscriberId } = req.params
+    
+    if (!mongoose.isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Channel id is invalid.")
+    }
+
+    const subscribedTo = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber: new mongoose.Types.ObjectId(subscriberId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                as: "subscribedTo",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                channel: {
+                    $first: "$subscribedTo"
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                channel: 1
+            }
+        }
+    ])
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                subscribedTo,
+                "Subscribed channels fectched successfully."
+            )
+        )
+})
+
+export { toggleSubscription, getUserChannelSubscribers , getSubscribedChannels }
